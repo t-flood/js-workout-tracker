a351cc9d8be504bacd5d37691e830fdc6d78fca8
Tim Flood <tflood9@gmail.com>
2021-03-16 22:28:46 -0400

    remove unused file

--- public/javascripts/Exercise.js
+++
@@ -1,9 +0,0 @@
-class Exercise {
-  constructor(attributes) {
-    this.name = attributes.name;
-    this.sets = attributes.sets;
-    this.reps = attributes.reps;
-    this.weight = attributes.weight;
-    this.notes = attributes.notes;
-  }
-}
